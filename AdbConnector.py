de73bb7d30b36c63c2d120c501450cd31ec43a55
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2024-11-13 18:55:14 -0500

    added new feature on displaying current screenshots for emulators on device selection ui

--- AdbConnector.py
+++ AdbConnector.py
@@ -1,8 +1,10 @@
 import subprocess
 import PathConverter
+import PIL.Image as Image
+import adbutils
 
 # Must call refresh_adb_device_list to update the dictionary before using
-serial_and_model_dict: dict[str, str] = {}
+serial_and_model_dict: dict[str, [str, Image.Image]] = {}
 
 
 def refresh_adb_device_list():
@@ -12,6 +14,8 @@ def refresh_adb_device_list():
     adb_device_result_list.pop(0)
     adb_device_result_list.pop()
     for adb_device_info in adb_device_result_list:
-        lst = adb_device_info.split(" ")
+        lst = adb_device_info.split(" ")   # lst[0] is the serial number
         model_name = next((ele for ele in lst if "model:" in ele), None)
-        serial_and_model_dict[model_name] = lst[0]
+        current_model_device = adbutils.device(lst[0])
+        current_device_image: Image = current_model_device.screenshot()
+        serial_and_model_dict[model_name] = [lst[0], current_device_image]
--- ui/DeviceSelection.py
+++ ui/DeviceSelection.py
@@ -3,6 +3,7 @@ import customtkinter as tk
 import AdbConnector
 from automation.Utilities import Utilities
 from ui.EpicSevenAutomationMain import MainWindow
+import PIL.Image as Image
 
 tk.set_appearance_mode("System")
 
@@ -19,11 +20,18 @@ class DeviceSelectionUI(tk.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.geometry("500x500")
         self.create_startup_widgets()
+        self.resizable(width=False, height=False)
 
     def create_startup_widgets(self):
         self.startup_label = tk.CTkLabel(self, text="default text")
         self.startup_label.grid(pady=10, sticky="nsew")
-        self.adb_connection_menu = tk.CTkOptionMenu(self)
+        self.device_screenshot_image = tk.CTkImage(light_image=Image.open("some.png"),
+                                                   size=(450, 250))
+        self.device_screenshot_label_holder = tk.CTkLabel(self, width=200, height=200, text="",
+                                                          image=self.device_screenshot_image)
+        self.device_screenshot_label_holder.grid(pady=10, sticky="nsew")
+        self.startup_label.grid(pady=10, sticky="nsew")
+        self.adb_connection_menu = tk.CTkOptionMenu(self, command=self.show_current_screenshot)
         self.adb_connection_menu.grid(pady=10, sticky="nsew")
         self.device_refresh_button = tk.CTkButton(self, text="Refresh Device List", command=self.refresh_device_ui)
         self.device_refresh_button.grid(pady=10, sticky="nsew")
@@ -35,15 +43,24 @@ class DeviceSelectionUI(tk.CTk):
     def refresh_device_ui(self):
         AdbConnector.refresh_adb_device_list()
         if AdbConnector.serial_and_model_dict:
+            mode_name_list = list(AdbConnector.serial_and_model_dict.keys())
             self.startup_label.configure(text="Please select device")
-            self.adb_connection_menu.configure(values=list(AdbConnector.serial_and_model_dict.keys()))
-            self.adb_connection_menu.set(list(AdbConnector.serial_and_model_dict.keys())[0])
+            for serial_number, image in AdbConnector.serial_and_model_dict.values():
+                print(f"serial_number: {serial_number}, image: {image}")
+            self.adb_connection_menu.configure(values=mode_name_list)
+            self.adb_connection_menu.set(mode_name_list[0])
             self.startup_button.configure(state="normal")
+            self.show_current_screenshot(mode_name_list[0])
         else:
             self.startup_label.configure(text="No device found, please click refresh button to fetch device again")
             self.adb_connection_menu.configure(values=list())
             self.adb_connection_menu.set(" ")
             self.startup_button.configure(state="disabled")
+
+    def show_current_screenshot(self, choice):
+        choice_image = AdbConnector.serial_and_model_dict[choice][1]
+        self.device_screenshot_image.configure(light_image=choice_image)
+        print(f"choice: {choice}, image: {AdbConnector.serial_and_model_dict[choice][1]}")
 
     def launch(self):
         self.mainloop()
@@ -51,4 +68,5 @@ class DeviceSelectionUI(tk.CTk):
     def launch_main_window(self):
         print("DeviceSelectionUI dying")
         self.destroy()
-        MainWindow(utilities=Utilities(AdbConnector.serial_and_model_dict[self.adb_connection_menu.get()])).launch()
+        MainWindow(
+            utilities=Utilities(AdbConnector.serial_and_model_dict[self.adb_connection_menu.get()][0])).launch()
