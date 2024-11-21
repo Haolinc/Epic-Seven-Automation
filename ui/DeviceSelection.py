dcaa273c8d47f866dcdff077953b226fe0789375
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2024-11-21 10:00:13 -0500

    add default image for startup device selection, and now user will see serial number of the device directly instead of device type

--- ui/DeviceSelection.py
+++ ui/DeviceSelection.py
@@ -1,13 +1,14 @@
 import customtkinter as tk
 
 import AdbConnector
+import PathConverter
 from automation.Utilities import Utilities
 from ui.EpicSevenAutomationMain import MainWindow
 import PIL.Image as Image
 
 tk.set_appearance_mode("System")
 
-
+default_image = Image.open(PathConverter.get_current_path("image", "NoImageAvailable.png"))
 class DeviceSelectionUI(tk.CTk):
     def __init__(self):
         super().__init__()
@@ -25,8 +26,7 @@ class DeviceSelectionUI(tk.CTk):
     def create_startup_widgets(self):
         self.startup_label = tk.CTkLabel(self, text="default text")
         self.startup_label.grid(pady=10, sticky="nsew")
-        self.device_screenshot_image = tk.CTkImage(light_image=Image.open("some.png"),
-                                                   size=(450, 250))
+        self.device_screenshot_image = tk.CTkImage(light_image=default_image, size=(450, 250))
         self.device_screenshot_label_holder = tk.CTkLabel(self, width=200, height=200, text="",
                                                           image=self.device_screenshot_image)
         self.device_screenshot_label_holder.grid(pady=10, sticky="nsew")
@@ -42,25 +42,23 @@ class DeviceSelectionUI(tk.CTk):
 
     def refresh_device_ui(self):
         AdbConnector.refresh_adb_device_list()
-        if AdbConnector.serial_and_model_dict:
-            mode_name_list = list(AdbConnector.serial_and_model_dict.keys())
+        if AdbConnector.serial_and_image_dict:
+            serial_name_list = list(AdbConnector.serial_and_image_dict.keys())
             self.startup_label.configure(text="Please select device")
-            for serial_number, image in AdbConnector.serial_and_model_dict.values():
-                print(f"serial_number: {serial_number}, image: {image}")
-            self.adb_connection_menu.configure(values=mode_name_list)
-            self.adb_connection_menu.set(mode_name_list[0])
+            self.adb_connection_menu.configure(values=serial_name_list)
+            self.adb_connection_menu.set(serial_name_list[0])
             self.startup_button.configure(state="normal")
-            self.show_current_screenshot(mode_name_list[0])
+            self.show_current_screenshot(serial_name_list[0])
         else:
             self.startup_label.configure(text="No device found, please click refresh button to fetch device again")
             self.adb_connection_menu.configure(values=list())
             self.adb_connection_menu.set(" ")
             self.startup_button.configure(state="disabled")
+            self.device_screenshot_image.configure(light_image=default_image)
 
     def show_current_screenshot(self, choice):
-        choice_image = AdbConnector.serial_and_model_dict[choice][1]
+        choice_image = AdbConnector.serial_and_image_dict[choice]
         self.device_screenshot_image.configure(light_image=choice_image)
-        print(f"choice: {choice}, image: {AdbConnector.serial_and_model_dict[choice][1]}")
 
     def launch(self):
         self.mainloop()
@@ -69,4 +67,4 @@ class DeviceSelectionUI(tk.CTk):
         print("DeviceSelectionUI dying")
         self.destroy()
         MainWindow(
-            utilities=Utilities(AdbConnector.serial_and_model_dict[self.adb_connection_menu.get()][0])).launch()
+            utilities=Utilities(self.adb_connection_menu.get())).launch()
