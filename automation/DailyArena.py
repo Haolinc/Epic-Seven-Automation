084ee8eeed98fc5d791288cfabd99b162a5394d5
ZeJun <zejunren1998@gmail.com>
2025-01-21 20:12:58 -0500

    adapt quick start feature for arena script add log fetching thread and make arena run on subprocess

--- automation/DailyArena.py
+++ automation/DailyArena.py
@@ -1,19 +1,14 @@
-import threading
 import time
-
 import PathConverter
-from automation.DailyArenaThread import DailyArenaThread
+
 from automation.Utilities import Utilities
-from ui.UIComponentEnum import *
 
 
 class DailyArena:
-    thread_shutdown = threading.Event()
 
-    def __init__(self, utilities: Utilities, listener):
+    def __init__(self, utilities: Utilities):
         self.utilities = utilities
-        self.ui_listener = listener
-        self.arena_thread = None
+        self.arena_process = None
         self.arena_icon = utilities.process_image_from_disk(PathConverter.get_current_path("image\\arena_asset", "Arena_Icon.png"))
         self.arena = utilities.process_image_from_disk(PathConverter.get_current_path("image\\arena_asset", "Arena.png"))
         self.NPC_challenge = utilities.process_image_from_disk((PathConverter.get_current_path("image\\arena_asset", "NPC_Challenge.png")))
@@ -26,21 +21,26 @@ class DailyArena:
         self.do_not_display = utilities.process_image_from_disk((PathConverter.get_current_path("image\\arena_asset", "Do_Not_Display_Button.png")))
         self.auto_battle_button = utilities.process_image_from_disk((PathConverter.get_current_path("image\\arena_asset", "Auto_Battle_Button.png")))
         self.confirm_Button = utilities.process_image_from_disk((PathConverter.get_current_path("image\\arena_asset", "Confirm_Button.png")))
-        
+        self.quick_start_button = utilities.process_image_from_disk((PathConverter.get_current_path("image\\arena_asset", "Quick_Start_Button.png")))
+        self.quick_confirm_button = utilities.process_image_from_disk((PathConverter.get_current_path("image\\arena_asset", "Quick_Confirm_Button.png")))
 
-    def select_arena(self):
-        if not self.utilities.wait_for_button_and_click_bool(self.arena,"find arena"):
-            self.ui_listener.add_label_to_log_frame("failed to find find arena, trying for next button")
-            if not self.utilities.wait_for_button_and_click_bool(self.NPC_challenge, "find npc challenge"):
-                self.ui_listener.add_label_to_log_frame("failed to find npc challenge, iteration stop")
+    def select_arena(self, log_queue):
+        if not self.utilities.wait_for_button_and_click_bool(self.NPC_challenge, "find npc challenge"):
+            log_queue.put("failed to find npc challenge, iteration stop")
 
     def challenge_opponent(self):
         self.utilities.wait_for_button_and_click(self.NPC_icon, "find NPC_icon")
         self.utilities.wait_for_button_and_click(self.challenge_button, "find challenge_button")
-        self.utilities.wait_for_button_and_click(self.start_button, "find start button")
-        self.gear_check_notification()
-        self.utilities.wait_for_button_and_click(self.auto_battle_button, "find auto_battle_button", 10, False)
-        self.utilities.wait_for_button_and_click(self.confirm_Button, "find confirm_Button",60)
+        time.sleep(2)
+        if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.quick_start_button)):
+            self.utilities.wait_for_button_and_click(self.quick_start_button, "find quick start button")
+            self.gear_check_notification()
+            self.utilities.wait_for_button_and_click(self.quick_confirm_button, "find quick_confirm_button")
+        else:
+            self.utilities.wait_for_button_and_click(self.start_button, "find start button")
+            self.gear_check_notification()
+            self.utilities.wait_for_button_and_click(self.auto_battle_button, "find auto_battle_button", 10, False)
+            self.utilities.wait_for_button_and_click(self.confirm_Button, "find confirm_Button",60)
 
     def buy_extra_flag(self):
         self.utilities.wait_for_button_and_click(self.arena_flag_icon, "find arena_flag_icon")
@@ -51,33 +51,19 @@ class DailyArena:
         if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.do_not_display)):
             self.utilities.wait_for_button_and_click(self.do_not_display, "Do_Not_Display_Button")
 
-    def arena_automation (self, shutdown_event):
-        total_iteration: int = self.ui_listener.get_entry_count(EntryEnum.ARENA_COUNT_ENTRY)
-        run_with_friendship_flag: bool = self.ui_listener.get_checkbox_bool(CheckBoxEnum.ARENA_WITH_FRIENDSHIP)
-        self.select_arena()
-        if run_with_friendship_flag:
-            total_iteration += 5
-            self.buy_extra_flag()
-        self.ui_listener.add_label_to_log_frame(text=f"--------Starting {total_iteration} npc challenge--------")
-        for current_iteration in range(total_iteration):
-            if shutdown_event.is_set():
-                self.ui_listener.add_label_to_log_frame(text="####### Process Stopped #######")
-                break
-            self.ui_listener.add_label_to_log_frame(text=f"--------Iteration: {current_iteration+1}--------")
-            self.challenge_opponent()
-            time.sleep(3)
-            self.ui_listener.add_label_to_log_frame(text=f"--------Iteration: {current_iteration+1} ended--------")
-
-    def start_daily_arena(self):
-        if self.arena_thread is not None and self.arena_thread.is_alive():
-            self.ui_listener.add_label_to_log_frame(text="####### Arena Thread Already Running #######")
-            return
-
-        self.arena_thread = DailyArenaThread(self, self.ui_listener)
-        self.arena_thread.start()
-
-    def stop_daily_arena(self):
-        if self.arena_thread is not None:
-            print(self.arena_thread.is_alive())
-            self.arena_thread.stop()
-            self.ui_listener.add_label_to_log_frame(text="####### Process Terminating #######")
+    def run_arena_automation_subprocess (self, log_queue, total_iteration, run_with_friendship_flag):
+        try:
+            self.select_arena(log_queue)
+            if run_with_friendship_flag:
+                total_iteration += 5
+                self.buy_extra_flag()
+            log_queue.put(f"Starting {total_iteration} npc challenge")
+            for current_iteration in range(total_iteration):
+                log_queue.put(f"Iteration: {current_iteration+1}")
+                self.challenge_opponent()
+                time.sleep(3)
+                log_queue.put(f"Iteration: {current_iteration+1} ended")
+            log_queue.put("Arena automation completed!")
+        except Exception as e:
+            log_queue.put(f"Error: {str(e)}")
+            log_queue.put("RESET_UI")
