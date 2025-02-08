5329dff01b6f1a64d11a57eaf59743046bd9d034
Haolinc <42785899+Haolinc@users.noreply.github.com>
2025-02-07 21:07:59 -0500

    Cache position (#26)
    * separate blur function and rename click_by_position to click_target_offset
    
    * now click will utilize cache to speed up click process, process image from disk will return a TaggedImage object, and fix several bugs on previous DailyArena changes
    
    * change shop identifier image and fix minor bug on counting covenant and mysitc purchase
    
    * fix minor bug that will accidentally clicked on bottom right disabling character skill due to cache click
    
    * fix bug on find image failure when quick match count is empty the first time
    
    * fix bug on missing future image on buying extra flag, and normal challenge will use correct target_img and identifier

--- automation/DailyArena.py
+++ automation/DailyArena.py
@@ -36,50 +36,66 @@ class DailyArena:
             (PathConverter.get_current_path("image\\arena_asset", "Do_Not_Display_Button.png")))
         self.auto_battle_button = utilities.process_image_from_disk(
             (PathConverter.get_current_path("image\\arena_asset", "Auto_Battle_Button.png")))
+        self.auto_battle_identifier = utilities.process_image_from_disk(
+            (PathConverter.get_current_path("image\\arena_asset", "Auto_Battle_Identifier.png")))
         self.confirm_Button = utilities.process_image_from_disk(
             (PathConverter.get_current_path("image\\arena_asset", "Confirm_Button.png")))
         self.quick_start_button = utilities.process_image_from_disk(
             (PathConverter.get_current_path("image\\arena_asset", "Quick_Start_Button.png")))
         self.quick_confirm_button = utilities.process_image_from_disk(
             (PathConverter.get_current_path("image\\arena_asset", "Quick_Confirm_Button.png")))
+        self.empty_quick_match = utilities.process_image_from_disk(
+            (PathConverter.get_current_path("image\\arena_asset", "Empty_Quick_Match.png")))
 
     def select_arena(self):
-        self.utilities.click_target(target_img=self.NPC_challenge,
-                                    future_target_img=self.NPC_challenge_identifier,
+        self.utilities.click_target(target_tagged_img=self.NPC_challenge,
+                                    future_tagged_imgs=self.NPC_challenge_identifier,
                                     identifier="find npc challenge")
 
     def challenge_opponent(self):
-        self.utilities.click_target(target_img=self.NPC_icon,
-                                    future_target_img=self.challenge_button, timeout=5, identifier="find NPC_icon")
-        self.utilities.click_target(target_img=self.challenge_button, future_target_img=self.match_window_identifier,
-                                    retry_count=5, color_sensitive=True, identifier="find challenge_button")
-        if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.quick_start_button)):
-            self.utilities.click_target(target_img=self.quick_start_button, identifier="find quick start button")
+        self.utilities.click_target(target_tagged_img=self.NPC_icon, future_tagged_imgs=self.challenge_button,
+                                    timeout=5, identifier="find NPC_icon")
+        self.utilities.click_target(target_tagged_img=self.challenge_button, future_tagged_imgs=self.match_window_identifier,
+                                    timeout=5, color_sensitive=True, identifier="find challenge_button")
+        if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.quick_start_button.image)):
+            self.utilities.click_target(target_tagged_img=self.quick_start_button,
+                                        future_tagged_imgs=[self.do_not_display, self.quick_confirm_button],
+                                        timeout=5, identifier="find quick start button")
             self.gear_check_notification()
-            self.utilities.click_target(target_img=self.quick_confirm_button,
-                                        future_target_img=self.NPC_challenge_identifier, timeout=10,
+            self.utilities.click_target(target_tagged_img=self.quick_confirm_button,
+                                        future_tagged_imgs=self.NPC_challenge_identifier, timeout=10, cache_click=False,
                                         color_sensitive=True, confidence=0.93, identifier="find quick_confirm_button")
         else:
-            self.utilities.click_target(target_img=self.start_button, identifier="find start button")
+            self.empty_quick_match_check()
+            self.utilities.click_target(target_tagged_img=self.start_button,
+                                        future_tagged_imgs=[self.do_not_display, self.auto_battle_button], timeout=5,
+                                        identifier="find start button")
             self.gear_check_notification()
-            self.utilities.click_target(target_img=self.auto_battle_button, timeout=10,
+            self.utilities.click_target(target_tagged_img=self.auto_battle_button,
+                                        future_tagged_imgs=self.auto_battle_identifier, timeout=10,
                                         identifier="find auto_battle_button")
-            self.utilities.click_target(target_img=self.quick_confirm_button,
-                                        future_target_img=self.NPC_challenge_identifier, timeout=60,
-                                        identifier="find quick_confirm_button")
+            self.utilities.click_target(target_tagged_img=self.confirm_Button,
+                                        future_tagged_imgs=self.NPC_challenge_identifier, timeout=60, cache_click=False,
+                                        identifier="find confirm_Button")
         time.sleep(3)   # Need around 3 seconds for animation
 
     def buy_extra_flag(self):
-        self.utilities.click_target(target_img=self.arena_flag_icon, future_target_img=self.friendship_point,
+        self.utilities.click_target(target_tagged_img=self.arena_flag_icon, future_tagged_imgs=self.friendship_point,
                                     identifier="find arena_flag_icon")
-        self.utilities.click_target(target_img=self.friendship_point, future_target_img=self.flag_buy_button,
+        self.utilities.click_target(target_tagged_img=self.friendship_point, future_tagged_imgs=self.flag_buy_button,
                                     identifier="find friendship_point")
-        self.utilities.click_target(target_img=self.flag_buy_button, identifier="find flag_buy_button")
+        self.utilities.click_target(target_tagged_img=self.flag_buy_button,
+                                    future_tagged_imgs=self.NPC_icon, identifier="find flag_buy_button")
 
     def gear_check_notification(self):
-        if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.do_not_display)):
-            self.utilities.click_target(target_img=self.do_not_display, future_target_img=self.quick_confirm_button,
+        if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.do_not_display.image)):
+            self.utilities.click_target(target_tagged_img=self.do_not_display, future_tagged_imgs=self.quick_confirm_button,
                                         identifier="Do_Not_Display_Button")
+
+    def empty_quick_match_check(self):
+        if bool(self.utilities.find_image(self.utilities.get_numpy_screenshot(), self.empty_quick_match.image)):
+            self.utilities.click_target(target_tagged_img=self.empty_quick_match, future_tagged_imgs=self.start_button,
+                                        identifier="Empty_Quick_Match")
 
     def run_arena_automation_subprocess(self, total_iteration, run_with_friendship_flag):
         try:
